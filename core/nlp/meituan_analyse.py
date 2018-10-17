51aceb36edfed3e8d78df4b4fd1ee44f927ff19f
wuwujun <sqq0103@gmail.com>
2018-10-17 23:46:27 +0800

    标签抽取异常捕捉记录

--- core/nlp/meituan_analyse.py
+++ core/nlp/meituan_analyse.py
@@ -7,6 +7,7 @@
 # @function: 对美团数据集应用百度api进行标签抽取
 
 import re
+import time
 import csv
 import pandas as pd
 from aip import AipNlp
@@ -23,28 +24,32 @@ def tag_extraction(data_save_path):
     options = {'type': 4}  # 餐饮美食类
     prop_adj = {}
     count = 0
-    df = pd.read_csv(data_save_path, encoding='utf-8')
-
-    for text in df.ix[:, 'content']:
-        count += 1
-        try:
-            result = client.commentTag(text, options)
-        except UnicodeError:
-            print('有编码问题的行数：' + str(count))
-            text = re.sub(u'([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])', ' ', text)  # 提取中文数字和英文
-            if text:
+    df = pd.read_csv(data_save_path, nrows=100, encoding='utf-8')
+    with open('../../results/meituan/error_log.txt', 'w+', encoding='utf-8') as f:
+        for text in df.ix[:, 'content']:
+            count += 1
+            try:
+                print(count)
+                # 提取中文数字和英文
+                text = re.sub(u'([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])', ' ', text)
                 result = client.commentTag(text, options)
+            except Exception as e:
+                f.write('有编码问题的行数：' + str(count))
+                print('有编码问题的行数：' + str(count))
+                print(e)
 
-        if 'items' in result:
-            data = result['items']
-            for item in data:
-                prop = item['prop']
-                adj = item['adj']
-                temp = prop_adj.setdefault(prop, set())  # 没有就添加且设置为value为空set，并且返回空set;有就返回不空的set，使用set是为了避免value中出现重复值
-                if adj:
-                    temp.add(adj)
-        else:
-            print(str(result['error_code']) + result['error_msg'])
+            if 'items' in result:
+                data = result['items']
+                for item in data:
+                    prop = item['prop']
+                    adj = item['adj']
+                    # 没有就添加且设置为value为空set，并且返回空set;有就返回不空的set，使用set是为了避免value中出现重复值
+                    temp = prop_adj.setdefault(prop, set())
+                    if adj:
+                        temp.add(adj)
+            else:
+                f.write('有错误的行数：' + str(count) + ' 错误代码：' + str(result['error_code']) + result['error_msg'])
+                print(str(result['error_code']) + result['error_msg'])
 
     tag = prop_adj.keys()
     adjs = list(prop_adj.values())
@@ -67,11 +72,14 @@ def read_csv():
 
 def main():
     file = 'sentiment_analysis_trainingset.csv'
-    path = '/home/wuwujun/Documents/Dataset/'
+    path = '/home/shuqianqian/dataset/'  # 服务器上的路径
     dc = Dataclean(filename=file, data_save_path=path)
     new_path = dc.clean_meituan()
     print('DONE==========================')
+    start = time.clock()
     tag_extraction(new_path)
+    elapsed = (time.clock() - start)
+    print('======================Time used: ', elapsed)
 
 
 if __name__ == '__main__':
